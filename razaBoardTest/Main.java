68905dc6fcd88e58bb722b5094e56811bc5ca045
Syed Raza <66960777+syedscerts@users.noreply.github.com>
2020-12-07 17:14:19 -0600

    Add files via upload

---
+++ razaBoardTest/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println();
+
+        Board boardTest = new Board();
+        boardTest.showBoard();
+
+
+        System.out.println();
+        System.out.println();
+    }
+}
